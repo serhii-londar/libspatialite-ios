f977ee16cc0901840060677d85b4b3f8e48291de
Iulian <iulian.tiganasu@ingr.ro>
2023-11-04 12:14:00 +0200

    fixes

--- run_make_framework.sh
+++ run_make_framework.sh
@@ -10,11 +10,11 @@ cp -v -R build/arm64-ios/include/*.h include
 
 # Make fat libraries for Simulator architectures 
 for file in build/arm64-sim/lib/*.a; \
-    do name=$(basename $$file .a); \
+    do name=$(basename $file .a); \
     lipo -create \
-        -arch arm64 build/arm64-sim/lib/$$name.a \
-        -arch x86_64 build/x86_64/lib/$$name.a \
-        -output lib/$$name.a \
+        -arch arm64 build/arm64-sim/lib/$name.a \
+        -arch x86_64 build/x86_64/lib/$name.a \
+        -output lib/$name.a \
     ; \
     done;
 ./make-framework
